e30e9cd494a37e3db2d9ee39a9ea1b8c5d8c0684
EC2 Default User <ec2-user@ip-172-31-43-109.ec2.internal>
2026-06-05 21:59:40 +0000

    major update

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import json 
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), ".." )))
 
@@ -10,26 +11,34 @@ from services.knowledge_loader import load_knowledge
 from services.plan_parser import extract_resources_changes
 from services.risk_parser import extract_risk_score
 from services.config_loader import load_config
+from services.scoring import calculate_risk_score
 
 with open("terraform/tfplan.json", 'r') as f:
     plan = f.read()
 
 resources_changes = extract_resources_changes(plan)
+
 knowledge = load_knowledge()
 prompt = build_review_prompt(resources_changes, knowledge)
 review = invoke(prompt)
-risk_score = extract_risk_score(review)
+
+deterministic_score = calculate_risk_score(resources_changes)
+ai_score = extract_risk_score(review)
+
+
+risk_score = deterministic_score   
 config = load_config()
 environment = config["environment"]
 risk_threshold = config["risk_thresholds"][environment]
 print(f"Risk Threshold: {risk_threshold}")
 print(review)
 print(f"Environment: {environment}")
+print(f"Deterministic Score: {deterministic_score}")
+print(f"AI Score: {ai_score}")
+print(f"Offical Risk Score: {risk_score}")
 
 with open("review.md", 'w') as f:
     f.write(review)
-
-print(f"Risk Score: {risk_score}")
 
 with open("risk_score.txt", "w") as f:
     f.write(str(risk_score))
@@ -37,6 +46,11 @@ with open("risk_score.txt", "w") as f:
 with open("risk_threshold.txt", "w") as f:
     f.write(str(risk_threshold))
 
+decision = "PASS" if risk_score < risk_threshold else "FAIL"
+print(f"Pipeline Decision: {decision}")
+
+with open("pipeline_decision.txt", "w") as f:
+    f.write(decision)
 #if risk_score >= 8:
  #   print("Critical risk detected. Failing pipeline.")
   #  exit(1)
---
+++ terraform/eks.tf
@@ -0,0 +1,35 @@
+resource "aws_eks_cluster" "eks_cluster" {
+  name = "ai-reviewer-cluster"
+  role_arn = aws_iam_role.eks_cluster_role.arn
+  vpc_config {
+    subnet_ids = [
+      aws_subnet.eks_subnet_1.id,
+      aws_subnet.eks_subnet_2.id
+    ]
+  }
+  depends_on = [ 
+    aws_iam_role_policy_attachment.eks_cluster_role_attachment
+   ]
+}
+
+resource "aws_eks_node_group" "eks_node_group" {
+  cluster_name = aws_eks_cluster.eks_cluster.name
+  node_group_name = "ai-reviewer-node-group"
+  node_role_arn = aws_iam_role.eks_node_role.arn
+  subnet_ids = [
+    aws_subnet.eks_subnet_1.id,
+    aws_subnet.eks_subnet_2.id
+  ]
+  scaling_config {
+    desired_size = 2
+    max_size = 3
+    min_size = 1
+  }
+  instance_types = ["t3.small"]
+
+  depends_on = [ 
+    aws_iam_role_policy_attachment.eks_node_role_attachment,
+    aws_iam_role_policy_attachment.eks_cni_policy_attachment,
+    aws_iam_role_policy_attachment.eks_ecr_pull_policy_attachment
+   ]
+}
